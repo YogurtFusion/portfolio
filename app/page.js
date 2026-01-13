3511438e5fd73c02acb9f527bb4d95f0589f6fd9
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-13 15:39:16 +0530

    hero half way

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import "./globals.css";
+
 
 export default function Home() {
   return (
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="hero min-h-screen">
-      <h1>Hero section</h1>
+    <section className="hero min-h-screen flex justify-between items-center mt-24  ">
+        <div className="flex  items-end justify-between  w-full">
+      <div
+        className="hero-head flex flex-col items-start justify-center mx-8  rounded-3xl  "
+        style={{
+          fontSize: "var(--fs-hero-h1) ",
+          lineHeight: "var(--lh-h1 )  ",
+          letterSpacing: "var(--ls-h1) "
+        }}
+      >
+        <h1 className=" text-[#1E1E1E] ">YELLOW </h1>
+        <h1 className="text-[#E6C35A] ">GLOW </h1>
+        <h1 className="text-[#1e1e1ecc] ">STUDIOS</h1>
+      </div>
+      <div className="hero-text flex flex-col items-end justify-center w-96 mb-8 px-4 py-2 rounded-3xl   " style={{
+        color: "var(--c-hero-p)",
+        fontSize: "var(--fs-hero-p) ",
+        letterSpacing: "var(--ls-p) ",
+        lineHeight: "var(--lh-p) "
+      }} >
+        Websites for restaurants that value speed, clarity, and customers.
+      </div>
+      </div>
     </section>
   );
 };

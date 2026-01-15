21af2666a7b77e44bceebca88320c39d2911041e
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-15 16:28:28 +0530

    project sturcture fixes complete ≧ ﹏ ≦

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+const servicesData = [
+  {
+    title: "Design",
+    description: "UI/UX design for websites and digital products.",
+  },
+  {
+    title: "Development",
+    description:
+      "Frontend & Backend development using modern frameworks, focused on performance and maintainability.",
+  },
+];
 const Services = () => {
   return (
-    <section className="p-12 flex flex-col justify-center  gap-20  border-b border-[#DBDAD6] ">
-      <div className="project-head flex justify-between items-center p-6 w-full ">
-        <div className="flex flex-col gap-3 w-full">
+    <section className="w-full max-w-336 mx-auto p-12 flex flex-col gap-20">
+      <div className="project-head grid grid-cols-1 md:grid-cols-2 gap-10 items-end pb-6   border-b border-[#DBDAD6]  ">
+        <div className="flex flex-col gap-2 ">
           <p
-            className=""
+            className=" uppercase tracking-widest "
             style={{
               fontSize: "var(--fs-sub-head)",
               color: "var(--c-sub-head) ",
@@ -22,76 +33,55 @@ const Services = () => {
               letterSpacing: "var(--ls-p) ",
             }}
           >
-            OUR
-          </h1>
-          <h1
-            className=""
-            style={{
-              color: "var(--c-h1) ",
-              fontSize: "var(--fs-head)",
-              lineHeight: "var(--lh-h1)",
-              letterSpacing: "var(--ls-p) ",
-            }}
-          >
-            SERVICES
+            OUR <br /> SERVICES
           </h1>
         </div>
         <div
-          className=""
+          className="md:max-w-md"
           style={{
             fontSize: "var(--fs-sub-head)",
             lineHeight: "var(--lh-p)",
             letterSpacing: "var(--ls-p)",
             color: "var(--c-p)",
           }}
-        ></div>
+        >
+          <p className="">
+          {/* <p className="text-base "> */}
+            We combine strategic thinking with meticulous craft to create
+            digital experiences that look beautiful and deliver results.
+          </p>
+        </div>
       </div>
-      <div className="Services-body  flex justify-between items-center border-t border-[#DBDAD6] gap-4 p-8">
-        <h2
-          className="font-light w-full"
-          style={{
-            fontSize: "var(--fs-h2)",
-            letterSpacing: "var(--ls-h2) ",
-            lineHeight: "var(--lh-h2)",
-            color: "var(--c-h2)",
-          }}
-        >
-          Design
-        </h2>
-        <p
-          className="w-full"
-          style={{
-            fontSize: "var(--fs-p)",
-            letterSpacing: "var(--ls-p)",
-            lineHeight: "var(--lh-p) ",
-          }}
-        >
-          UI/UX design for websites and digital products.
-        </p>
-      </div>
-      <div className="Services-body  flex justify-between items-center border-t border-[#DBDAD6] gap-4 p-8">
-        <h2
-          className="font-light w-full"
-          style={{
-            fontSize: "var(--fs-h2)",
-            letterSpacing: "var(--ls-h2) ",
-            lineHeight: "var(--lh-h2)",
-            color: "var(--c-h2)",
-          }}
-        >
-          Development
-        </h2>
-        <p
-          className="w-full"
-          style={{
-            fontSize: "var(--fs-p)",
-            letterSpacing: "var(--ls-p)",
-            lineHeight: "var(--lh-p) ",
-          }}
-        >
-          Frontend & Backend development using modern frameworks, focused on
-          performance and maintainability.
-        </p>
+      <div className="Services-body  flex flex-col">
+        {servicesData.map((service, index) => (
+          <div
+            key={index}
+            className="group py-8   border-b border-[#DBDAD6] grid grid-cols-1 md:grid-cols-2 gap-4 items-center "
+          >
+            <h2
+              className="font-light w-full"
+              style={{
+                fontSize: "var(--fs-h2)",
+                letterSpacing: "var(--ls-h2) ",
+                lineHeight: "var(--lh-h2)",
+                color: "var(--c-h2)",
+              }}
+            >
+              {service.title}
+            </h2>
+
+            <p
+              className="w-full"
+              style={{
+                fontSize: "var(--fs-p)",
+                letterSpacing: "var(--ls-p)",
+                lineHeight: "var(--lh-p) ",
+              }}
+            >
+              {service.description}
+            </p>
+          </div>
+        ))}
       </div>
     </section>
   );

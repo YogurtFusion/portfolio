17c557470a86d74eedee7bdbfa3b825dca5c6ed9
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-15 23:49:30 +0530

    About section completed

--- components/ui/SectionBtn.jsx
+++ components/ui/SectionBtn.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const SectionBtn = () => {
   return (
     <button className=" group inline-flex items-center gap-2 text-[#121212] ">
-      <span className="tracking-[0.01em] relative ">
-        view all project
-        <span className="absolute left-0 -bottom-1 h-px w-0 bg-[#121212] transition-all duration-300 group-hover:w-full "></span>
+      <span className=" tracking-widest uppercase text-sm font-medium relative ">
+        view all projects
+        <span className="absolute left-0 -bottom-1 h-px w-full bg-[#121212] origin-left transform scale-x-0  transition-all duration-300 group-hover:scale-x-100 "></span>
       </span>
       <span className="material-symbols-outlined group-hover:translate-x-1 transition-transform duration-300  ">
         arrow_forward

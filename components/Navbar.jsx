127ee3265422e2bcd2ec6db48cc3a8c25897e500
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-18 08:28:29 +0530

    nav optimization

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import NavBtn from "./ui/NabBtn";
+import NavBtn from "./ui/NavBtn";
 
 const Navbar = () => {
   return (
-    <nav className="  bg-(--nav-bg)  fixed top-6 left-0 right-0 z-50 backdrop-blur-md shadow-sm rounded-full h-16 md:max-w-6xl max-w-xl  w-full mx-auto flex justify-between items-center px-6 border border-neutral-200 ">
+    <nav className="   fixed top-6 left-1/2 -translate-x-1/2  z-50 backdrop-blur-md shadow-sm rounded-full h-16 md:max-w-6xl max-w-xl w-[90%] lg:w-full mx-auto flex justify-between items-center px-4 md:px-6 border border-neutral-200 ">
       {/* logo */}
-      <div className=" flex  items-center gap-3 ">
-        <div className=" flex items-center justify-center text-xs font-serif w-8 h-8 rounded-full border border-black  ">
-          YG
+      <Link href={"/"} className=" group flex  items-center gap-3 ">
+        <div className=" flex items-center justify-center text-xs font-serif w-9 h-9 rounded-full border border-neutral-900 group-hover:scale-105 transition-transform  ">
+           YG
         </div>
         {/* logo-text */}
         <span className="  font-light  tracking-tight hidden md:block text-lg ">
           Yellow Glow Studios
         </span>
-      </div>
-      <ul className="flex items-center justify-between gap-8  text-sm font-medium text-neutral-600">
+      </Link>
+      <ul className=" hidden md:flex items-center justify-between gap-8  text-sm font-medium text-neutral-600 ">
         <li>
           <Link href={"/"} className="hover:text-black transition-colors">
             Home
@@ -33,11 +33,14 @@ const Navbar = () => {
           <Link href={"#about"} className="hover:text-black transition-colors">
             About Us
           </Link>
-        </li>
-        <li>
-          <NavBtn />
-        </li>
+        </li> 
+       
+         
+     
       </ul>
+      <div>
+      <NavBtn />
+      </div>
     </nav>
   );
 };
--- components/ui/NavBtn.jsx
+++ components/ui/NavBtn.jsx
@@ -5,7 +5,7 @@ const NavBtn = () => {
   return (
     <a
       href="mailto:yellowglowstudios@gmail.com?subject=Website%20project%20inquiry&body=Hi%20Yellow%20Glow%20Studios,%0D%0A%0D%0AI’m%20interested%20in%20a%20website%20for%20my%20business.%0D%0A%0D%0AHere%20are%20a%20few%20quick%20details:%0D%0A•%20Business%20type:%0D%0A•%20Timeline:%0D%0A•%20Budget%20range%20(optional):%0D%0A%0D%0ALooking%20forward%20to%20hearing%20from%20you.%0D%0A%0D%0AThanks,"
-      className="group relative inline-flex items-center uppercase rounded-full  bg-neutral-900 text-neutral-50 px-5 py-2.5  text-xs font-medium tracking-wide transition-all duration-300 ease-out hover:translate-y-[-1px] hover:shadow-md active:translate-y-0 "
+      className="group relative inline-flex items-center uppercase rounded-full  bg-neutral-900 text-neutral-50 px-5 py-2.5  text-xs font-medium tracking-wide transition-all duration-300 ease-out hover:translate-y-[-1px] hover:shadow-md active:translate-y-0  w-fit md:w-full"
     >
       <span>Start project</span>
       <span className="absolute h-[1px] w-[70%] -translate-x-1/2 scale-x-0 group-hover:scale-x-100 origin-center left-1/2 right-0 bottom-2 bg-neutral-50 transform transition-transform will-change-transform duration-300  pointer-events-none "></span>
